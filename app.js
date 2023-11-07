693d31dd5ed848a22de3f692499c5d5d1177600c
Yashina Gomes <yashinagm@gmail.com>
2023-11-07 20:28:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const db = require('./models/db');
+
+app.get('/', async(req, res) => {
+  res.send('Página 1 Inicial');
+});
+
+app.listen(8080, () =>{
+  console.log('Servidor iniciado: http://localhost:8080');
+});
